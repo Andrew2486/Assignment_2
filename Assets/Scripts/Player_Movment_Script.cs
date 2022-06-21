5d0be782c1f4879ac2b514aa4e03c8e7d6fcd413
Andrew Law <Hybr1dserpent@hotmail.com>
2022-06-22 00:07:02 +1200

    game
    v3.2

--- Assets/Scripts/Player_Movment_Script.cs
+++ Assets/Scripts/Player_Movment_Script.cs
@@ -4,30 +4,76 @@ using UnityEngine;
 
 public class Player_Movment_Script : MonoBehaviour
 {
+   // public bool flipX;
+    private SpriteRenderer sprite;
     private Animator anim;
-    public float SPEED = 0.02f;
+    public float SPEED = 40f;
     public Vector2 motion;
     Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
+        Move();
+        movement();
+    }
+    public void movement()
+    {
 
 
-         gameObject.transform.position = new Vector2(transform.position.x + (h * SPEED),
-         transform.position.y + (v * SPEED));
-         //anim.Play("Player_Walk");
-        //Frozen Constraints on Rigidbody to stop sliding
-
-
-        
-
+        //Diagonal movement
+        if (Input.GetKey(KeyCode.A) && (Input.GetKey(KeyCode.W)))
+        {
+            motion = Vector2.left + Vector2.up;
+            sprite.flipX = true;
+        }
+        else if (Input.GetKey(KeyCode.A) && (Input.GetKey(KeyCode.S)))
+        {
+            motion = Vector2.left + Vector2.down;
+            sprite.flipX = true;
+        }
+        else if (Input.GetKey(KeyCode.D) && (Input.GetKey(KeyCode.W)))
+        {
+            motion = Vector2.up + Vector2.right;
+            sprite.flipX = false;
+        }
+        else if (Input.GetKey(KeyCode.D) && (Input.GetKey(KeyCode.S)))
+        {
+            motion = Vector2.down + Vector2.right;
+            sprite.flipX = false;
+        }//Single way movement
+        else if (Input.GetKey(KeyCode.A))
+        {
+            motion = Vector2.left;
+            sprite.flipX = true;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            motion = Vector2.right;
+            sprite.flipX = false;
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            motion = Vector2.up;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            motion = Vector2.down;
+        }
+        else
+        {
+            motion = Vector2.zero;
+        }
+    }
+    public void Move()
+    {
+        rb.velocity = motion * SPEED * Time.deltaTime;
     }
 }
